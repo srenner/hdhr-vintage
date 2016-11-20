3bf592984c3be642d7e282267e3939fab0b4bbce
Shawn Renner <resume@shawnrenner.com>
2016-11-19 23:47:51 -0600

    fetch tuners POC

--- hdhr-vintage/MainWindow.xaml.cs
+++ hdhr-vintage/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,43 @@ namespace hdhr_vintage
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        //todo fetch this from the textbox
+        private string ConfigExecutable = "C:\\Program Files\\Silicondust\\HDHomeRun\\hdhomerun_config.exe";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnTunerScan_Click(object sender, RoutedEventArgs e)
+        {
+            string discoverCommand = ConfigExecutable + " discover";
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss.fff") + " - " + "performing tuner scan");
+
+            var proc = new Process();
+            proc.StartInfo.FileName = ConfigExecutable;
+            proc.StartInfo.Arguments = "discover";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            string output = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            int exitCode = proc.ExitCode;
+            proc.Close();
+
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss.fff") + " - " + "tuner scan output (exit code " + exitCode + ") - " + output);
+
+            UpdateStatusBarText(output);
+        }
+
+        private void UpdateStatusBarText(string text)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                statusText.Text = text.Trim();
+            });
         }
     }
 }
